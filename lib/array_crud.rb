c1a42ff860da789a8e48cfd45c63ce80546da394
sensible-shell-8529 <diana.lee116@gmail.com>
2019-02-01 23:43:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array= ["JT","Adele", "Beyonce", "Ariana"]
+ ["JT","Adele", "Beyonce", "Ariana"]
 end
 
 def add_element_to_end_of_array(array, element)
